f8eebe45b149a7c8378cd1ee1744b7c7d5a6cb6b
Voold <barsik.koteevich@mail.ru>
2025-11-14 06:55:58 +0700

    React && TS template

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Logo from '../public/logo.svg'
+
+function App() {
+
+  return (
+    <div className={''}><img className='logo' src={Logo}></img>
+      <h1>ТУТ БУДЕТ ФРОНТЕНД</h1>
+      <h2>Vavilonus10 || React && TS</h2>
+    </div>
+  )
+}
+
+export default App
